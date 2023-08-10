3dad1fa11b093b07fe5dd49eb809a8dc0069cbbc
Dominic8999 <dominic3331@fedora.email>
2023-08-10 20:36:58 +0200

    better

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
+let buttons = {
+    click: document.getElementById("click"),
+    save: document.getElementById("save-btn"),
+    reset: document.getElementById("refresh-btn")
+}
 let saveEl = document.getElementById("saved")
 let countEl = document.getElementById("count-el")
-
-
 let count = 0
+let countStr = " "
 
-function clicked() {
+buttons.click.addEventListener("click", function() {
     count += 1
     countEl.textContent = count
     console.log("The current number is: " + count)
-}
+})
 
-let countStr = " "
-
-function save() {
+buttons.save.addEventListener("click",function() {
     countStr = count + ", "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
     console.log("The item was saved.")
-}
+})
 
-function reset() {
+buttons.reset.addEventListener("click",function() {
     countStr = "reseted"
     saveEl.textContent = countStr
     countEl.textContent = 0
     count = 0
     saveEl.textContent = "Previous entered: "
     console.log("the items were reseted")
-}
+})
